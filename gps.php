eb25f0fb9905ea9df6505e7d091078883e97e091
LDV <palacex@gmail.com>
2015-06-11 13:51:52 +0300

    test git codesniffer+git hook

--- gps.php
+++ gps.php
@@ -18,7 +18,7 @@ include_once(DIR_MODULES."application.class.php");
 $session = new session("prj");
 
 // connecting to database
-$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); 
+$db =new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); 
 
 include_once("./load_settings.php");
 
